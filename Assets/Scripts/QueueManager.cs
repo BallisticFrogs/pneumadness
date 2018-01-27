19dd6b5cf01f94613f0e44cb1be2794eeabba857
vbeauzee <vincentbeauzee@gmail.com>
2018-01-27 20:14:58 +0100

    Implement QueueManager Behavior

--- Assets/Scripts/QueueManager.cs
+++ Assets/Scripts/QueueManager.cs
@@ -1,16 +1,86 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR.WSA.Persistence;
 
-public class QueueManager : MonoBehaviour {
+public class QueueManager : MonoBehaviour
+{
+    public static QueueManager INSTANCE;
 
-	// Use this for initialization
-	void Start () {
-		
+    private Queue<PipeTile> pipeQueue = new Queue<PipeTile>();
+
+    private List<KeyValuePair<PipeTile, int>> availableTubeWithProba = new List<KeyValuePair<PipeTile, int>>();
+
+    private Random random = new Random();
+
+    public PipeTile tubeBendDr;
+    public PipeTile tubeBendDl;
+    public PipeTile tubeBendUl;
+    public PipeTile tubeBendUr;
+    public PipeTile tubeCross;
+    public PipeTile tubeStraightV;
+    public PipeTile tubeStraightH;
+    public PipeTile tubeTD;
+    public PipeTile tubeTL;
+    public PipeTile tubeTR;
+    public PipeTile tubeTU;
+
+    // Use this for initialization
+    void Start ()
+    {
+        INSTANCE = this;
+
+        availableTubeWithProba.Add(new KeyValuePair<PipeTile, int>(tubeBendDr, 9));
+        availableTubeWithProba.Add(new KeyValuePair<PipeTile, int>(tubeBendDl, 9));
+        availableTubeWithProba.Add(new KeyValuePair<PipeTile, int>(tubeBendUl, 9));
+        availableTubeWithProba.Add(new KeyValuePair<PipeTile, int>(tubeBendUr, 9));
+        availableTubeWithProba.Add(new KeyValuePair<PipeTile, int>(tubeStraightV, 22));
+        availableTubeWithProba.Add(new KeyValuePair<PipeTile, int>(tubeStraightH, 22));
+        availableTubeWithProba.Add(new KeyValuePair<PipeTile, int>(tubeTD, 4));
+        availableTubeWithProba.Add(new KeyValuePair<PipeTile, int>(tubeTL, 4));
+        availableTubeWithProba.Add(new KeyValuePair<PipeTile, int>(tubeTR, 4));
+        availableTubeWithProba.Add(new KeyValuePair<PipeTile, int>(tubeTU, 4));
+        availableTubeWithProba.Add(new KeyValuePair<PipeTile, int>(tubeCross, 4));
+
+        for (int i = 0; i < 5; i++)
+        {
+            pipeQueue.Enqueue(GetRandomTubeWithProba());
+        }
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+    public Queue<PipeTile> getQueue()
+    {
+        return pipeQueue;
+    }
+
+    public PipeTile DequeuePipe()
+    {
+        pipeQueue.Enqueue(GetRandomTubeWithProba());
+        return pipeQueue.Dequeue();
+    }
+
+    private PipeTile GetRandomTubeWithProba()
+    {
+        PipeTile selectedTile = null;
+
+        int diceRoll = Random.Range(0, 100);
+
+        int cumulative = 0;
+        for (int i = 0; i < availableTubeWithProba.Count; i++)
+        {
+            cumulative += availableTubeWithProba[i].Value;
+            if (diceRoll < cumulative)
+            {
+                selectedTile =  availableTubeWithProba[i].Key;
+                break;
+            }
+        }
+        return selectedTile;
+    }
 }
--- Assets/Scripts/UserActionManager.cs
+++ Assets/Scripts/UserActionManager.cs
@@ -9,8 +9,6 @@ public class UserActionManager : MonoBehaviour {
 
     private Tilemap tilemapObj;
 
-    public PipeTile tubeTile;
-
     public Grid grid;
 
     void Awake()
@@ -29,13 +27,23 @@ public class UserActionManager : MonoBehaviour {
         if (Input.GetMouseButtonUp(0))
         {
             Vector3 mousepos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            Debug.Log("mousepos = " + mousepos);
             Vector3 cellPosition = grid.WorldToCell(mousepos);
             Vector3Int cellPositionInt = Vector3Int.CeilToInt(cellPosition);
-            Debug.Log("cellPosition = " + cellPositionInt);
+                
+            PipeTile pipe = QueueManager.INSTANCE.DequeuePipe();
 
-            tilemapObj.SetTile(cellPositionInt, tubeTile);
-            tilemapObj.RefreshTile(cellPositionInt);
+            if (tilemapObj.GetTile(cellPositionInt) == null)
+            {
+                tilemapObj.SetTile(cellPositionInt, pipe);
+                tilemapObj.RefreshTile(cellPositionInt);
+                //TODO play success tube placement
+            }
+            else
+            {
+                //TODO play error tube placement
+            }
+
+            
         }
   
     }
