06f02670692624e64bea987b193c4a000c0ed9ec
vbeauzee <vincentbeauzee@gmail.com>
2018-01-28 12:51:15 +0100

    Add ambient sound.

--- Assets/Scripts/SoundManager.cs
+++ Assets/Scripts/SoundManager.cs
@@ -10,9 +10,61 @@ public class SoundManager : MonoBehaviour
     public AudioSource bgmSource;
     public AudioSource fxSource;
 
+    public AudioClip random_atmosphere_1;
+    public AudioClip random_atmosphere_2;
+    public AudioClip random_atmosphere_3;
+    public AudioClip random_atmosphere_4;
+    public AudioClip random_atmosphere_5;
+    public AudioClip random_atmosphere_6;
+    public AudioClip random_atmosphere_7;
+    public AudioClip random_atmosphere_8;
+    public AudioClip random_atmosphere_9;
+    public AudioClip random_atmosphere_10;
+    public AudioClip random_atmosphere_11;
+    public AudioClip random_atmosphere_12;
+    public AudioClip random_atmosphere_13;
+
+    private int randomMusic  = 0;
+    private float randomTime = 120f;
+    private float timeCounter = 0f;
+    private List<AudioClip> ambientClip;
+
     private void Awake()
     {
         INSTANCE = this;
+    }
+
+    private void Start()
+    {
+        ambientClip = new List<AudioClip>();
+        ambientClip.Add(random_atmosphere_1);
+        ambientClip.Add(random_atmosphere_2);
+        ambientClip.Add(random_atmosphere_3);
+        ambientClip.Add(random_atmosphere_4);
+        ambientClip.Add(random_atmosphere_5);
+        ambientClip.Add(random_atmosphere_6);
+        ambientClip.Add(random_atmosphere_7);
+        ambientClip.Add(random_atmosphere_8);
+        ambientClip.Add(random_atmosphere_9);
+        ambientClip.Add(random_atmosphere_10);
+        ambientClip.Add(random_atmosphere_11);
+        ambientClip.Add(random_atmosphere_12);
+        ambientClip.Add(random_atmosphere_13);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timeCounter > randomTime && !fxSource.isPlaying)
+        {
+            fxSource.Stop();
+            randomTime = Random.Range(5000f, 20000f);
+            timeCounter = 0f;
+            randomMusic = Random.Range(1, 13);
+            fxSource.clip = ambientClip[randomMusic];
+            fxSource.Play();
+        }
+        timeCounter += Time.deltaTime;
     }
 
     public void PlayFx(AudioClip clip)
